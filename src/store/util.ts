22317327850d96b93973c5ab8738c0eaa928cc1f
Andrew Melnik <andreew.mel@gmail.com>
2022-12-13 20:33:40 +0300

    tags splitting by '#' added

--- src/store/util.ts
+++ src/store/util.ts
@@ -1,4 +1,4 @@
 export const extractTags = (input: string) => {
-  return Array.from(new Set(input.split(' ').filter((word) => word.startsWith('#'))));
+  return Array.from(new Set(input.split(' ').map((el) => (el[0] === '#' ? el : `#${el}`))));
 };
 
